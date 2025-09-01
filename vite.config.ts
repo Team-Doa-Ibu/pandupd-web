b1075e4b3b1b544011b93643fc8f1ddd28d53622
NekoMorie <davihanan11@gmail.com>
2025-09-01 13:23:41 +0700

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
       routes(defineRoutes) {
         return flatRoutes("routes", defineRoutes, {
           ignoredRouteFiles: ["**/.*"], // Ignore dot files (like .DS_Store)
-          //appDir: 'app',
+          //appDir: 'app',ll
           //routeDir: 'routes',
           //basePath: '/',
           //paramPrefixChar: '$',
